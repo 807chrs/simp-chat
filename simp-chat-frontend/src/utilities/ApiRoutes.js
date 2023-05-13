408e475a4e7231e4cb3681b64d8a69b841f81a2d
ChrisT1207 <122280762+ChrisT1207@users.noreply.github.com>
2023-05-14 01:43:36 +0800

    Removed unused page

--- simp-chat-frontend/src/utilities/ApiRoutes.js
+++ simp-chat-frontend/src/utilities/ApiRoutes.js
@@ -1,7 +1,6 @@
 export const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
-// export const setProfileRoute = `${host}/api/auth/set-profile`;
 export const allUsersRoute = `${host}/api/auth/allUsers`;
 export const sendMessageRoute = `${host}/api/messages/addMessage`;
 export const getAllMessages = `${host}/api/messages/getAllMessages`;
